f8e532bddac0dd481809665b938aa276a098ea73
AyunGaming <brian.verchere@gmail.com>
2026-07-15 14:16:54 +0200

    Fix Markdown renderer import

--- src/app/redaction/page.tsx
+++ src/app/redaction/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FormEvent, useEffect, useState } from "react";
+import MarkdownContent from "@/components/MarkdownContent";
 import {
   ApiError,
   ContentItem,
